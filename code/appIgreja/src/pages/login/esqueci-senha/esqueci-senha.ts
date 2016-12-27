2167100a3d4eac55535b044a9d35e3034d766c3b
Gabriel Reis Miranda <grmiranda97@gmail.com>
2016-12-26 23:43:44 -0300

    esqueci senha com validação

--- code/appIgreja/src/pages/login/esqueci-senha/esqueci-senha.ts
+++ code/appIgreja/src/pages/login/esqueci-senha/esqueci-senha.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, ToastController} from 'ionic-angular';
+import { Validators, FormBuilder } from '@angular/forms';
 
 /*
   Generated class for the EsqueciSenha page.
@@ -13,10 +14,55 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EsqueciSenhaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  email;
+
+  constructor(private toastCtrl: ToastController, public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, public alertCtrl: AlertController) {
+    //Configurando objeto user com campos para validação
+    this.email = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EsqueciSenhaPage');
   }
 
+  validate(): boolean {
+    if (this.email.valid) {
+      return true;
+    }
+
+    // figure out the error message
+    let errorMsg = '';
+
+    // validate each field
+    let control = this.email.controls['email'];
+    if (!control.valid) {
+      if (control.errors['required']) {
+        errorMsg = 'Informe um email';
+      } else if (control.errors['minlength']) {
+        errorMsg = 'Informe um email válido';
+      }
+    }
+
+    let alert = this.alertCtrl.create({
+      title: 'Erro!',
+      subTitle: errorMsg || 'Empty error message!',
+      buttons: ['OK']
+    });
+    alert.present();
+
+    return false;
+  }
+
+  enviar(){
+    if (this.validate()) {
+      // process the data
+      let toast = this.toastCtrl.create({
+        message: 'Email foi enviado',
+        duration: 2000
+      });
+      toast.present();
+    }
+  }
 }
