80256775eabaca7630f0b4b2aca908beeb817205
Gabriel Reis Miranda <grmiranda97@gmail.com>
2017-01-06 16:14:29 -0300

    editar e deletar sliding

--- code/appIgreja/src/pages/add-post/add-post.ts
+++ code/appIgreja/src/pages/add-post/add-post.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Publicacao } from '../../model/publicacao';
 import { PublicacaoService } from '../../providers/publicacao-service';
+import { ImagePicker } from 'ionic-native';
+import { Camera } from 'ionic-native';
 
 
 @Component({
@@ -11,6 +13,7 @@ import { PublicacaoService } from '../../providers/publicacao-service';
 export class AddPostPage {
 
   private publicacao: Publicacao = new Publicacao();
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public postService: PublicacaoService) {
 
@@ -26,5 +29,26 @@ export class AddPostPage {
         console.log(res.message);
       }
     });
+  }
+
+  importarFoto(){
+    let options;
+    ImagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+      console.log('Image URI: ' + results[i]);
+    }
+  }, (err) => { });
+  }
+
+  tirarFoto(){
+    let options;
+    Camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+
   }
 }
