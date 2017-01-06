1d05b656bca626500c08c262e0f1e94e56aab67d
lucas <lukspaiva95@gmail.com>
2017-01-06 15:43:19 -0300

    adicionando publicacao

--- code/appIgreja/src/model/publicacao.ts
+++ code/appIgreja/src/model/publicacao.ts
@@ -3,7 +3,7 @@ export class Publicacao {
   public IDPublicacao: number;
   public TempoPermanencia: Date;
   public Comentario: boolean;
-  public LinkImagem: string = "";
+  public LinkImagem: string;
   public Titulo: string;
   public Texto: string;
   public Usuario_IDUsuario: number = 1;
