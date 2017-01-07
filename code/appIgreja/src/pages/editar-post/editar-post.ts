22570bb1bced814185999abe3fd67dfc454afd68
Gabriel Reis Miranda <grmiranda97@gmail.com>
2017-01-06 21:51:44 -0300

    tela editar publicação pronta

--- code/appIgreja/src/pages/editar-post/editar-post.ts
+++ code/appIgreja/src/pages/editar-post/editar-post.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ImagePicker } from 'ionic-native';
+import { Camera } from 'ionic-native';
 
 /*
   Generated class for the EditarPost page.
@@ -19,4 +21,27 @@ export class EditarPostPage {
     console.log('ionViewDidLoad EditarPostPage');
   }
 
+  importarFoto(){
+    let options;
+    ImagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+      console.log('Image URI: ' + results[i]);
+    }
+  }, (err) => { });
+  }
+
+  tirarFoto(){
+    let options;
+    Camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  salvar(){
+    this.navCtrl.pop();
+  }
 }
