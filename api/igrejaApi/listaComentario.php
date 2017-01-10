83af1212fdd884744bd4ac95a9a702c72c6aadc6
lucas <lukspaiva95@gmail.com>
2017-01-10 16:02:14 -0300

    nome de usuario no comentario

--- api/igrejaApi/listaComentario.php
+++ api/igrejaApi/listaComentario.php
@@ -12,6 +12,17 @@
 			$sql = "SELECT * FROM comentario WHERE Publicacao_IDPublicacao = '$id'";
 			$result = $con->query($sql);
 			while($row=$result->fetch_assoc()){
+				$userId = $row['Usuario_IDUsuario'];
+					
+				$sql = "SELECT * FROM usuario WHERE IDUsuario = '$userId'";
+				$result2 = $con->query($sql);
+					
+				$row2 = $result2->fetch_assoc();
+					
+				$nome = $row2['Nome'];
+					
+				$row['username'] = $nome;
+					
 				$vetor[] = $row;
 			}
 			echo json_encode($vetor);
