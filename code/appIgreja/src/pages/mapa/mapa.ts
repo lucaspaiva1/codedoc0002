7a06172fe5d0766a237c11e89ce0ff24ddb7f467
Gabriel Reis Miranda <grmiranda97@gmail.com>
2017-01-17 22:38:47 -0300

    add ngCordova (npm install please)

--- code/appIgreja/src/pages/mapa/mapa.ts
+++ code/appIgreja/src/pages/mapa/mapa.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, CameraPosition, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
+import { Geolocation, GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, CameraPosition, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
 
 /*
   Generated class for the Mapa page.
@@ -8,6 +8,7 @@ import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, CameraPosition, GoogleMap
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
 @Component({
   selector: 'page-mapa',
   templateUrl: 'mapa.html'
@@ -19,50 +20,5 @@ export class MapaPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapaPage');
   }
-
-  // Load map only after view is initialize
-  ngAfterViewInit() {
-    this.loadMap();
-  }
-
-  loadMap() {
- // make sure to create following structure in your view.html file
- // <ion-content>
- //  <div #map id="map"></div>
- // </ion-content>
-
- // create a new map by passing HTMLElement
- let element: HTMLElement = document.getElementById('map');
-
- let map = new GoogleMap(element);
-
- // listen to MAP_READY event
- map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
-
- // create LatLng object
- let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
-
- // create CameraPosition
- let position: CameraPosition = {
-   target: ionic,
-   zoom: 18,
-   tilt: 30
- };
-
- // move the map's camera to position
- map.moveCamera(position);
-
- // create new marker
- let markerOptions: GoogleMapsMarkerOptions = {
-   position: ionic,
-   title: 'Ionic'
- };
-
- map.addMarker(markerOptions)
-   .then((marker: GoogleMapsMarker) => {
-      marker.showInfoWindow();
-    });
- }
-
 
 }
