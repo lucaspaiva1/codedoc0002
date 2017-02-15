4d36a9f5a460a3ebb15c48a7feff34475fa3aa13
lucas paiva <lukspaiva95@gmail.com>
2017-02-15 00:46:32 -0300

    'alterandofotoperfil'

--- code/appIgreja/src/pages/perfil/perfil.ts
+++ code/appIgreja/src/pages/perfil/perfil.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ActionSheetController, NavParams, AlertController, Platform, ToastController, LoadingController, Loading } from 'ionic-angular';
 import { Events } from 'ionic-angular';
-import { ImagePicker, Camera, File, Transfer, FilePath } from 'ionic-native';
+import { Camera } from 'ionic-native';
 import { User } from '../../model/User';
 import { FacebookService } from '../../providers/facebook-service';
 import { UserService } from '../../providers/user-service';
@@ -18,14 +18,11 @@ export class PerfilPage {
 
   private editar: boolean = false;
   private user: User = new User();
-  private novaImagem: string = null;
-  private urlImage: string = null;
   private loading: Loading;
   private loaded: boolean = false;
   private senhaAtual: string = '';
   private confSenha1: string = '';
   private confSenha2: string = '';
-
 
   constructor(
     private toastCtrl: ToastController,
@@ -43,7 +40,6 @@ export class PerfilPage {
 
     this.userService.get().then(response => {
       this.user = response;
-      this.urlImage = this.user.URLFoto;
       this.loaded = true;
     });
   }
@@ -72,7 +68,7 @@ export class PerfilPage {
   }
 
   private alterarSenha(): boolean {
-    if(this.senhaAtual !== ''){
+    if (this.senhaAtual !== '') {
       if (this.user.Senha.trim() !== this.senhaAtual) {
         this.presentToast('Senha Atual Inválida');
         return false;
@@ -85,28 +81,15 @@ export class PerfilPage {
         this.user.Senha = this.confSenha1;
         return true;
       }
-    }else{
+    } else {
       return true;
     }
   }
 
   private salvar() {
     if (this.validate() && this.alterarSenha()) {
-      let equals: boolean;
-      if (this.urlImage == this.user.URLFoto) {
-        this.user.linkAntigo = '';
-        equals = true;
-      } else {
-        //link antigo usado para apagar a foto de perfil nao-utilizada
-        this.user.linkAntigo = this.user.URLFoto;
-        this.user.URLFoto = 'http://www.dsoutlet.com.br/igrejaApi/perfil/' + this.novaImagem;
-        equals = false;
-      }
       this.contaService.editar(this.user).then(response => {
         if (response) {
-          if (equals == false) {
-            this.uploadImage();
-          }
           this.userService.set(this.user);
           this.presentToast('Modificações Salvas');
           this.editar = false;
@@ -182,56 +165,36 @@ export class PerfilPage {
   }
 
   private importarFoto() {
-    this.takePicture(Camera.PictureSourceType.PHOTOLIBRARY);
-  }
-
-  private tirarFoto() {
-    this.takePicture(Camera.PictureSourceType.CAMERA);
-  }
-
-  private takePicture(sourceType) {
-    // Create options for the Camera Dialog
-    let options = {
-      quality: 100,
-      sourceType: sourceType,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
-    };
-
-    // Get the data of an image
-    Camera.getPicture(options).then((imagePath) => {
-      // Special handling for Android library
-      if (this.platform.is('android') && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
-        FilePath.resolveNativePath(imagePath)
-          .then(filePath => {
-            let currentName = filePath.substr(imagePath.lastIndexOf('/') + 1);
-            let correctPath = filePath.substr(0, imagePath.lastIndexOf('/') + 1);
-            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-            this.urlImage = filePath;
-          });
-      } else {
-        let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-        let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-        this.urlImage = imagePath;
-      }
-    }, (err) => {
-      this.presentToast('Erro ao selecionar imagem.');
+    Camera.getPicture({
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      saveToPhotoAlbum: false
+    }).then(imageData => {
+      this.user.URLFoto = "data:image/jpeg;base64," + imageData;
+    }, error => {
+      alert("ERROR -> " + JSON.stringify(error));
     });
   }
 
-  // Create a new name for the image
-  private createFileName() {
-    let time = new Date();
-    return (time.getTime() + ".jpg");
-  }
-
-  // Copy the image to a local folder
-  private copyFileToLocalDir(namePath, currentName, newFileName) {
-    File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-      this.novaImagem = newFileName;
+  private tirarFoto() {
+    Camera.getPicture({
+      quality: 50, //Picture quality in range 0-100. Default is 50
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.user.URLFoto = "data:image/jpeg;base64," + imageData;
     }, error => {
-      this.presentToast('Erro ao arquivar imagem.');
+      alert("ERROR -> " + JSON.stringify(error));
     });
   }
 
@@ -243,42 +206,6 @@ export class PerfilPage {
       position: 'top'
     });
     toast.present();
-  }
-
-  //responsável por upload da imagem
-  private uploadImage() {
-    // Destination URL
-    let url = "http://www.dsoutlet.com.br/igrejaApi/uploadPerfil.php";
-
-    // File for Upload
-    //let targetPath = this.pathForImage(this.novaImagem);
-
-    // File name only
-    let filename = this.novaImagem;
-
-    let options = {
-      fileKey: "file",
-      fileName: filename,
-      chunkedMode: false,
-      mimeType: "multipart/form-data",
-      params: { 'fileName': filename }
-    };
-
-    const fileTransfer = new Transfer();
-
-    this.loading = this.loadingCtrl.create({
-      content: 'Carregando...',
-    });
-    this.loading.present();
-
-    // Use the FileTransfer to upload the image
-    fileTransfer.upload(this.urlImage, url, options).then(data => {
-      this.loading.dismissAll();
-      this.presentToast('Edição Concluida!');
-    }, err => {
-      this.loading.dismissAll()
-      this.presentToast('Erro no envio da imagem.');
-    });
   }
 
 }
