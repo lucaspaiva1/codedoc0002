48e24a1e9cc9fc53b17932b78b9a73bcd06fea98
Havallon <havallon.ecomp@gmai.com>
2017-02-14 23:06:32 -0300

    Deletando grupo

---
+++ api/igrejaApi/deleteGrupo.php
@@ -0,0 +1,35 @@
+<?php
+	include 'mySQL.php';
+	require 'mySQL.php';
+?>
+
+<?php
+	
+	$the_request = &$_POST;
+
+	$postdata = file_get_contents("php://input");
+	
+	if (isset($postdata)){
+		$request = json_decode($postdata);
+		
+		$id = $request->id;
+		
+		$sql = "SELECT * FROM grupo WHERE ID = '$id'";
+		$result = $con->query($sql);
+		
+		$numrow = $result->num_rows;
+		
+		if ($numrow !== 1){
+			echo json_encode(false);
+		} else {
+			$sql = "DELETE FROM representagrupo WHERE grupo_ID = '$id'";
+			$con->query($sql);
+			
+			$sql = "DELETE FROM grupo WHERE ID = '$id'";
+			$con->query($sql);
+			
+			echo json_encode(true);
+		}
+		$con->close();
+	}
+?>
