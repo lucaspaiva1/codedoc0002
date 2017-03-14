66af72e456ec9171e969bb0c440adfcdf7ed13b5
cleybson cardoso <cleybsonc@gmail.com>
2017-03-14 20:43:03 -0300

    tela de contatos

--- api/igrejaApi/contato.php
+++ api/igrejaApi/contato.php
@@ -11,13 +11,13 @@
 		$row1->Nome = "Paulo Marcos";
 		$row1->Email = "oluapsocram_vita@hotmail.com";
 		$row1->Foto = "http://www.dsoutlet.com.br/igrejaApi/imagens/anonimo.png";
-		$row1->Face = "https://www.facebook.com/cleybson.cardoso";
+		$row1->Face = "https://www.facebook.com/paulomarcos.sampaio";
 		$vetor[] = $row1;
 
-		$row1->Nome = "Paulo Marcos";
-		$row1->Email = "oluapsocram_vita@hotmail.com";
+		$row1->Nome = "Maila Aguiar";
+		$row1->Email = "mailaaguiar2@gmail.com";
 		$row1->Foto = "http://www.dsoutlet.com.br/igrejaApi/imagens/anonimo.png";
-		$row1->Face = "https://www.facebook.com/cleybson.cardoso";
+		$row1->Face = "https://www.facebook.com/maila.aguiar";
 		$vetor[] = $row1;
 
 
