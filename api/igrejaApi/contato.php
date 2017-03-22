c8b0b06f66277aafce8eabd7201864d42d27ad56
Havallon <havallon.ecomp@gmail.com>
2017-03-22 17:40:27 +0000

    fixed contato.php

--- api/igrejaApi/contato.php
+++ api/igrejaApi/contato.php
@@ -14,12 +14,13 @@
 		$row1->Face = "https://www.facebook.com/paulomarcos.sampaio";
 		$vetor[] = $row1;
 
-		$row1->Nome = "Maila Aguiar";
-		$row1->Email = "mailaaguiar2@gmail.com";
-		$row1->Foto = "http://www.dsoutlet.com.br/igrejaApi/imagens/anonimo.png";
-		$row1->Face = "https://www.facebook.com/maila.aguiar";
-		$vetor[] = $row1;
-
+		$row2->Nome = "Maila Aguiar";
+		$row2->Email = "mailaaguiar2@gmail.com";
+		$row2->Foto = "http://www.dsoutlet.com.br/igrejaApi/imagens/anonimo.png";
+		$row2->Face = "https://www.facebook.com/maila.aguiar";
+		$vetor[] = $row2;
+		
+	
 
 
 
